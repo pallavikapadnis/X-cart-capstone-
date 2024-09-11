a787df78cd31f46ca65f0cfaa0f26c73320dbe0b
pallavikapadnis <96579750+pallavikapadnis@users.noreply.github.com>
2024-09-11 21:09:29 +0530

    Delete cypress/e2e/homepage.spec.cy.js

--- cypress/e2e/homepage.spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('X-Cart Demo Homepage', () => {
-    it('should load the homepage successfully', () => {
-      cy.visit('https://demo.x-cart.com/demo/home.php');
-      cy.contains('Featured products'); // Check for a specific element on the homepage
-    });
-  });
