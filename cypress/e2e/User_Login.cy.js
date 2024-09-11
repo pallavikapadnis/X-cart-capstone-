9fabd90eff658bd6ebf8d0a2a002df15904c8655
pallavikapadnis <96579750+pallavikapadnis@users.noreply.github.com>
2024-09-11 21:09:00 +0530

    Delete cypress/e2e/User_Login.cy.js

--- cypress/e2e/User_Login.cy.js
+++
@@ -1,9 +0,0 @@
-describe('X-Cart Demo User Login', () => {
-    it('should login with valid credentials', () => {
-        cy.fixture('user_data').then((data) => {
-            const user = data.validUser;
-            cy.login(user.email, user.password);
-        });
-    });
-});
-
